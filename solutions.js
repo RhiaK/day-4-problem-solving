a0e371020935df4d4ca5b3617a7d99bd0e2c999b
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-22 19:21:00 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,4 @@
+//2.
+//A parameter is what will become a variable when specified, the argument is the value of that variable that has been specified.
+//3.
+//Return, returns a completed answer, console.log is a print function that spits out whatever you tell it to.
